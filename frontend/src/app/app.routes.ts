568f5d636e9724d73c074e3b1fd0e84eb8e4fd91
DEVIT SAH <dsah_be22@thapar.edu>
2026-09-17 11:08:25 +0530

    Add tutorials route to app routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RoutineComponent } from './components/routine/routine.component';
 import { NoticesPageComponent } from './components/notices-page/notices-page.component';
+import { TutorialsComponent } from './components/tutorials/tutorials.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'notices', component: NoticesPageComponent },
   { path: 'routine', component: RoutineComponent },
+  { path: 'tutorials', component: TutorialsComponent },
   { path: '**', redirectTo: '' },
 ];
